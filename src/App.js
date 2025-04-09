4a1ff4d90c6ff412254a7d2690c3b8c899f6473b
Cielo Roman <croman12@depaul.edu>
2025-04-09 12:13:14 -0500

    fix url route attempt 5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import { Link, HashRouter, Route } from 'react-router-dom';
+import { Link, HashRouter, Route, Routes } from 'react-router-dom';
 import Starter from './components/Starter';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <HashRouter>
-        <Route path="/start" component={Starter} />
-      </HashRouter>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <h1>Hello</h1>
+        <Routes>
+          <Route path="/start" element={<Starter />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
